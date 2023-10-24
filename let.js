f96b960bef94900f3cc11819b094f800e1ff256d
João Victor <jdallachiesaa@gmail.com>
2023-10-24 14:32:37 -0300

    Update let.js

--- let.js
+++ let.js
@@ -1,5 +1,25 @@
-let altura = 5;
-let comprimento = 10;
+//let forma = 'retangulo'
+//let altura = 5;
+//let comprimento = 10;
+//let area;
 
-area = altura * comprimento
-console.log (area)
+    // if (forma === 'retangulo') {
+    //    area = altura * comprimento;
+    // } else {
+    //    area = (altura * comprimento) /2;
+    // }
+
+    //console.log (area)
+
+    const forma = 'Quadrado';
+    const altura = 5;
+    const comprimento = 7;
+    let area;
+
+    if (forma === 'quadrado') {
+        area = altura * comprimento ;
+    } else {
+        area = (altura * comprimento) /2;
+    }
+    console.log (area)
+
